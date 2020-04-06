fd79d9c5c6a8d961e92a4ec6be12aab036190996
Flight15 <63256974+Flight15@users.noreply.github.com>
2020-04-06 21:03:21 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,4 +14,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Shut up you low elo trash')
 
-client.run('Njk2NzY4ODc3OTQxNjIwODE3.XotpjA.d26SyhammfiAZrCAQ00U8RswqPw')
+client.run('')
